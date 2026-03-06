38e6dc94fac8cceb34c38d8ffbda3bbb162b0bb5
Glaceon127 <961886476@qq.com>
2026-03-06 23:05:06 +0800

    添加服务端，合并目录

---
+++ server/CServer.cpp
@@ -0,0 +1,27 @@
+#include "CServer.h"
+#include "HttpConnection.h"
+CServer::CServer(boost::asio::io_context& ioc, 
+	unsigned short& port): _ioc(ioc), _acceptor(ioc, tcp::endpoint(tcp::v4(), port)), _socket(ioc) {
+
+}
+
+void CServer::Start() {
+	auto self = shared_from_this();
+	_acceptor.async_accept(_socket, [self](beast::error_code ec) {
+		try {
+			//出错放弃该链接,继续监听其他链接
+			if (ec) {
+				self->Start();
+				return;
+			}
+			//创建新连接，并且创建HttpConnection类管理链接
+			std::make_shared<HttpConnection>(std::move(self->_socket))->Start();
+
+			//继续监听
+			self->Start();
+		}
+		catch (std::exception& exp) {
+		
+		}
+		});
+}
